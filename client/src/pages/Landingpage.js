107d581c963f6e9b0b8b72bf19419ce96a3f6c28
unknown <payalashtikar@gmail.com>
2023-12-18 23:01:57 +0530

    explaination

--- client/src/pages/Landingpage.js
+++ client/src/pages/Landingpage.js
@@ -3,7 +3,12 @@ import React from 'react'
 const LandingPage = () => {
     return (
         <div className=''>
-            <h1 className='text-6xl m-28'>Welcome</h1>
+            {/* <h1 className='text-6xl m-28'>Welcome</h1> */}
+            <img
+                className='w-[100%] bg-cover'
+                src='https://static.vecteezy.com/system/resources/previews/010/925/820/non_2x/colorful-welcome-design-template-free-vector.jpg'
+                alt='landingimg'
+            />
         </div>
     )
 }
